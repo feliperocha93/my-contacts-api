8a0bfa818dee4fda691c8637e14651d1c945572d
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-16 19:58:56 -0300

    put method was created

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -29,9 +29,8 @@ class ContactController {
     }
 
     const contactExistst = await ContactsRepository.findByEmail(email);
-
     if (contactExistst) {
-      return response.status(400).json({ error: 'This e-mail is already been taken' });
+      return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -41,8 +40,31 @@ class ContactController {
     response.send(contact);
   }
 
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
 
+    const contactExistst = await ContactsRepository.findById(id);
+    if (!contactExistst) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
