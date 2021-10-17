2343fc2529f5eeb94c8cf6d2d4383ca9ac9666a7
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-17 12:14:42 -0300

    refactoring of the repository delete method and removing of the uuid dependency

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Felipe Rocha de Lucena',
-    email: 'fee.rocha@hotmail.com',
-    phone: '11999999999',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Lebron James',
-    email: 'lj@nike.com',
-    phone: '947-8736',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -60,11 +41,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
