c7da5e2be703483f7ffe8a8786b12487898aeed9
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-17 14:47:58 -0300

    error handler implemented

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,11 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  console.log('#### Error Handler');
+  console.log(error);
+  response.status(500).send();
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
