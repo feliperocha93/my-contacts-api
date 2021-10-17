b2fce3d9862f8f67a4a0c348b606921f67be1dfb
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-17 13:32:22 -0300

    show, update and delete routes were created

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -7,6 +7,14 @@ class CategoryController {
     response.json(categories);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    response.json(category);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
@@ -17,6 +25,32 @@ class CategoryController {
     const category = await CategoriesRepository.create({ name });
 
     response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const contactExistst = await CategoriesRepository.findById(id);
+    if (!contactExistst) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -6,6 +6,11 @@ class CategoriesRepository {
     return rows;
   }
 
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM categories WHERE id = $1', [id]);
+    return row;
+  }
+
   async create({ name }) {
     const [row] = await db.query(`
       INSERT INTO categories(name)
@@ -14,6 +19,21 @@ class CategoriesRepository {
     `, [name]);
 
     return row;
+  }
+
+  async update(id, { name }) {
+    const [row] = await db.query(`
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *
+    `, [name, id]);
+    return row;
+  }
+
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM categories WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
